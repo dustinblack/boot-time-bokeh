3e3e4af46191e21d58da1e1414a1e852740719ea
Dustin Black <dblack@redhat.com>
2023-09-20 10:41:20 +0200

    re-factor, comment, and improve highlights

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource, HoverTool, LabelSet
 from bokeh.io import output_notebook
 import copy
 import json
 import re
 
-source_file = "dblack_test_09_15_2023_18_46_39.json"
+source_file = "dblack_test_09_18_2023_12_11_40.json"
 
 f = open(source_file)
 
 boot_time_data = json.load(f)
 
+# Data structure to be used by Bokeh
 data = {
     "id": [],
     "start": [],
     "duration": [],
     "end": [],
     "name": [],
+    "label": [],
     "log_source": [],
     "color": [],
     "bar_height": [],
 }
 
+# Copy of the data structure to hold the parallel sorted lists
 sorted_data = copy.deepcopy(data)
 
+# Min/Max time values to filter results on
 min_duration = 0
 max_duration = 100000
-max_index = 20000
+max_start_time = 20000
 
+# Standard visualization formatting
 dmesg_color = "blue"
 systemd_color = "red"
+standard_bar_height = 1
+chart_width = 1000
+chart_height = 700
+
+# Highlight formatting based on regular expression match
+highlight_re_pattern = "multi-user.target|initrd-switch-root"
 highlight_color = "green"
-standard_height = 1
-highlight_height = 100
-highlight_pattern = "multi-user.target"
+highlight_bar_height = 2
 
-n = 1
-
+# Parse the data file for metrics of interest
 for item in boot_time_data:
     for i in item["timing_details"]:
-        if (min_duration <= i["time"] / 1000 <= max_duration and i["activating"] / 1000 < max_index) or re.search(
-                highlight_pattern, i["name"]):
+        if (min_duration <= i["time"] / 1000 <= max_duration and i["activating"] / 1000 < max_start_time) or re.search(
+                highlight_re_pattern, i["name"]):
             data["start"].append(round(i["activating"] / 1000, 3))
             data["duration"].append(round(i["time"] / 1000, 3))
             data["end"].append(round((i["activating"] + i["time"]) / 1000, 3))
@@ -49,44 +57,50 @@ for item in boot_time_data:
                 data["log_source"].append("systemd")
             else:
                 data["log_source"].append("dmesg")
-            if re.search(highlight_pattern, i["name"]):
+            if re.search(highlight_re_pattern, i["name"]):
                 data["color"].append(highlight_color)
-                data["bar_height"].append(highlight_height)
+                data["bar_height"].append(highlight_bar_height)
+                data["label"].append(i["name"])
             else:
                 if "activated" in i:
                     data["color"].append(systemd_color)
                 else:
                     data["color"].append(dmesg_color)
-                data["bar_height"].append(standard_height)
-            n += 1
+                data["bar_height"].append(standard_bar_height)
+                data["label"].append(None)
 
 
-sstart, sduration, send, sname, slog_source, scolor, sbar_height = (
+# Parallel-sort the data structure lists based on start time
+(
+    sorted_data["start"],
+    sorted_data["duration"],
+    sorted_data["end"],
+    sorted_data["name"],
+    sorted_data["label"],
+    sorted_data["log_source"],
+    sorted_data["color"],
+    sorted_data["bar_height"]
+) = (
     list(t) for t in zip(*sorted(zip(
         data["start"],
         data["duration"],
         data["end"],
         data["name"],
+        data["label"],
         data["log_source"],
         data["color"],
         data["bar_height"],
     )))
 )
 
-sorted_data["start"] = sstart
-sorted_data["duration"] = sduration
-sorted_data["end"] = send
-sorted_data["name"] = sname
-sorted_data["log_source"] = slog_source
-sorted_data["color"] = scolor
-sorted_data["bar_height"] = sbar_height
-
-
+# Enumerate the log items sequentially after sort
 for id, n in enumerate(sorted_data["start"]):
     sorted_data["id"].append(id)
 
+# Convert the data into a ColumnDataSource for Bokeh
 source = ColumnDataSource(data=sorted_data)
 
+# Configure the tool-tip display on mouse hover
 hover = HoverTool(
     tooltips=[
         ("Source", "@log_source"),
@@ -98,15 +112,42 @@ hover = HoverTool(
     point_policy="follow_mouse",
 )
 
-output_notebook()
-
-p = figure(title=f"Boot Time Measurements -- {len(sorted_data['id'])} Actions -- {source_file}", y_axis_label="Boot Action (Sequence ID)",
-           x_axis_label="Time Since Start (ms)", width=1000, height=700)
-p.hbar(y="id", left="start", right="end", source=source, color="color", height="bar_height")
+# Build the Bokeh chart
+p = figure(
+    title=f"Boot Time Measurements -- {len(sorted_data['id'])} Actions -- {source_file}",
+    y_axis_label="Boot Action (Sequence ID)",
+    x_axis_label="Time Since Start (ms)",
+    width=chart_width,
+    height=chart_height,
+)
+p.hbar(
+    y="id",
+    left="start",
+    right="end",
+    source=source,
+    color="color",
+    height="bar_height",
+)
 p.tools.append(hover)
 p.y_range.flipped = True
 p.title.text_font_size = '15pt'
 p.axis.major_label_text_font_size = "15pt"
 p.axis.axis_label_text_font_size = "15pt"
 
+# Label the highlighted items
+labels = LabelSet(
+    x="start",
+    y="id",
+    text="label",
+    x_offset=5,
+    y_offset=5,
+    source=source,
+    text_color=highlight_color,
+    background_fill_color="white",
+    border_line_color=highlight_color,
+    border_line_width=1,
+)
+p.add_layout(labels)
+
+output_notebook()
 show(p)
